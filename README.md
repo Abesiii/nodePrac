f75085fc9bc8f7b5ec636ef5e9a472954fd75ef6
kksshh0612 <kksshh0612@gmail.com>
2022-11-27 17:46:12 +0900

    오직 mysql 연동만

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # nodePrac
 
 ## vsc 터미널에서 node project 실행하는 방법 
-
+<br>
+--------get-post-study(branch)의 내용 --------
 ### 터미널에서 
 #### node init : 노드 프로젝트 시작
 #### nodeserver : 노드 기반 프로젝트를 하기 위한 기본 설정 파일(package.json) 만들어짐. 
 #### npm install express : 노드 기반 웹 서버를 제공하는 프레임워크 express install. 
-
 --------여기까지 하면 node_modules라는 디렉토리 생김. --------
-
 #### npm install nodemon -g : 코드가 바뀔 때마다 웹 서버 재시작. node 어쩌구.js 안해도 됨. nodemon 어쩌구.js 이렇게 시작하면 됨. 근데 이거 안되네? 
 #### npm install body-parser : request와 response 사이에서 파싱을 해주는 미들웨어. request의 body 부분을 파싱해줌. 
 #### npm install ejs : ejs 사용할 수 있게 해줌 
+--------get-post-study(branch)의 내용 --------
+<br><br>
+--------mysql-connection(branch)의 내용--------
+#### npm install mysql : mysql 모듈 설치
+--------mysql-connection(branch)의 내용--------
--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');   //node_modules에 있는 express에 관련된 함수 모듈을 가져옴(객체는 아님. 반환값 함수)
 var app = express();        
 var bodyParser = require('body-parser');        
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({     //mysql connection 생성 
+  host : 'localhost',
+  port : 3306,
+  user : 'root',
+  password : 'kksshh1735',
+  database : 'node_prac'        //데이터베이스 이름
+});
+connection.connect();       //mysql 연동
+
 
 app.listen(3000, function(){             //콜백함수는 비동기. 서버 켜질때까지 기다렸다가 실행되는 것 아니고, 다음 코드 먼저 실행되고 그 다음에 콜백함수 실행됨. 
   console.log("3000포트로 서버 시작");    //node의 디폴트 포트는 3000
@@ -34,6 +45,23 @@ app.post('/email_post', function(req, res){
 //from.html에서 ajax로 json 데이터를 보냄.
 app.post('/ajax_send_email', function(req, res){
   var email = req.body.email;       //전송받은 json의 key를 써줘야 함. 
-  var responseData = {'result' : 'ok', 'email' : email};      //json 데이터를 만들고
-  res.json(responseData);                                     //json으로 변환하여 response함. 
+  // var responseData = {'result' : 'ok', 'email' : email};      //json 데이터를 만들고
+  var responseData = {};
+
+  var query = connection.query("select name from user where email='" + email + "';", function(err, rows){
+    if(err) return err;
+    else{
+      if(rows[0]){
+        responseData.result = "ok";
+        responseData.name = rows[0].name;
+        console.log(responseData);
+      }
+      else{
+        responseData.result = "none";
+        responseData.name = "";
+        console.log(responseData);
+      }
+    }
+    res.json(responseData);   //json으로 변환하여 response함. 
+  })                                 
 })
